89a9dc06ab4984e1d4d94f3c48ac85a82d0a9dff
AlgorithmChopda <vinaychopda4@gmail.com>
2023-09-16 09:00:07 +0530

    feat: handlebars

---
+++ src/services/mail/SendAlertMail.ts
@@ -0,0 +1,30 @@
+import sendMail from './sendMail';
+const SendAlertMail = async (
+  email: string,
+  message: string,
+  username: string,
+) => {
+  const CLIENT_BASE_URL = process.env['CLIENT_BASE_URL'];
+  if (!CLIENT_BASE_URL) {
+    throw new Error('CLIENT_BASE_URL not Defined');
+  }
+
+  const verificationURL = `${CLIENT_BASE_URL}/dashboard/inventory`;
+  const senderEmail = process.env['MAIL_USER'];
+  const emailSubject = 'Inventory ALert';
+  const emailTemplate = 'alert';
+  const officialName = 'Hack - X';
+  const context = { verificationURL, officialName, message, username };
+
+  const mailOptions = {
+    from: senderEmail,
+    to: email,
+    subject: emailSubject,
+    template: emailTemplate,
+    context: context,
+  };
+
+  return await sendMail(mailOptions);
+};
+
+export default SendAlertMail;
